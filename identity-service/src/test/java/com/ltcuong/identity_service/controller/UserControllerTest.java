c472d40ca66273971a9c112afd84a72b9b325976
Le the cuong <cuonglethe2k4@gmail.com>
2024-11-29 21:14:16 +0700

    add class UserControllerTest and dependency json-type

--- identity-service/src/test/java/com/ltcuong/identity_service/controller/UserControllerTest.java
+++ identity-service/src/test/java/com/ltcuong/identity_service/controller/UserControllerTest.java
@@ -1,4 +1,88 @@
 package com.ltcuong.identity_service.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ltcuong.identity_service.dto.request.UserCreationRequest;
+import com.ltcuong.identity_service.dto.response.UserResponse;
+import com.ltcuong.identity_service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private LocalDate dob;
+
+    // chạy 1 lần trước khi các hàm test gọi đến
+    @BeforeEach
+    // hàm tạo data
+    void initData(){
+        dob = LocalDate.of(2004,9,7);
+
+        request = UserCreationRequest.builder()
+                .username("johnDoe")
+                .firstname("john")
+                .lastname("Doe")
+                .password("12345678")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("4f428467df01")
+                .username("johnDoe")
+                .firstname("John")
+                .lastname("Doe")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception{
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createUser(ArgumentMatchers.any()))
+                .thenReturn(userResponse);
+        // WHEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(999))
+                .andExpect(MockMvcResultMatchers.jsonPath("data.id")
+                        .value("4f428467df01")
+        );
+        // THEN
+    }
+
+
 }
